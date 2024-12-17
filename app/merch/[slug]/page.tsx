0c74375cc3e87fa2fad2fe1f6da116058af63587
klhaug <kristian@snougle.no>
2024-12-17 11:15:28 +0100

    merchpage desklayout done

--- app/merch/[slug]/page.tsx
+++ app/merch/[slug]/page.tsx
@@ -11,14 +11,16 @@ export default async function MerchPage({params}: {params: Promise<{slug: string
     console.log(correctMerch)
 
     return(
-        <div className="m-auto max-w-[1728px] h-[600px] md:h-[800px]">
-            <div>
-                <div className="flex justify-center h-screen items-center max-w-[1260px] gap-8">
-                    <Image src={correctMerch[0].image} alt={correctMerch[0].title} width={250} height={250} />
-                    <div className="flex flex-col">
-                        <h1 className="font-sec text-5xl font-regular">Takk for at du ønsker å kjøpe vinyl!</h1>
-                        <p className="font-sec">Betalingsløsningen min er dessverre ikke tilgjengelig enda, så ta kontakt på<b> mads.gjetmundsen@gmail.com</b> eller <b>+46 902 35 356</b> så finner vi en løsning!</p>
-                        <Link className="flex justify-center items-center h-11 px-6 bg-blue-main hover:bg-blue-hover text-white" href={"/"} >Tilbake</Link>
+        <div className="m-auto">
+            <div className=" max-w-[1728px] h-[600px] md:h-[800px] mx-24 flex  justify-center items-center">
+                <div className="max-w-[1300px] flex flex-wrap gap-8 items-center md:flex-nowrap" >
+                    <div className="md:min-w-[300px]">
+                        <Image src={correctMerch[0].image} alt={correctMerch[0].title} width={350} height={350} />
+                    </div>
+                    <div className="flex flex-col gap-4 justify-center max-w-[715px]">
+                        <h1 className="font-sec text-4xl font-regular">Takk for at du ønsker å kjøpe vinyl!</h1>
+                        <p className="font-sec leading-8 tracking-wide font-regular">Betalingsløsningen min er dessverre ikke tilgjengelig enda, så ta kontakt på<b> mads.gjetmundsen@gmail.com</b> eller <b>+46 902 35 356</b> så finner vi en løsning!</p>
+                        <Link className="flex md:max-w-[175px] justify-center items-center h-11 px-6 bg-blue-main hover:bg-blue-hover text-white" href={"/"}> ⬅ Tilbake</Link>
                     </div>
             </div>
             </div>
